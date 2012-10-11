70f36439e6f02b5bbfb1dba586af99f871eb9c25
Mathias Fussenegger <f.mathias@zignar.net>
2012-10-11 19:45:14 +0200

    pep8 formatting; catch keyboardinterrupt

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
 import tornado.web
 import tornado.httpserver
 import tornado.ioloop
@@ -17,33 +18,35 @@ from config import cookie_secret
 
 define('port', default=8080, help='run on the given port', type=int)
 
+
 class Application(tornado.web.Application):
     def __init__(self):
         static_path = join(dirname(__file__), 'static')
 
         handlers = (
-                (r'/', Index)
-                ,(r'/feeds/edit/([0-9]+)/?', FeedEdit)
-                ,(r'/feeds/delete/([0-9]+)/?', FeedDelete)
-                ,(r'/feeds/add/?', FeedAdd)
-                ,(r'/refresh/?', Refresh)
-                ,(r'/mark_as_read/?', MarkAsRead)
-                ,(r'/static/(.*)', tornado.web.StaticFileHandler,
-                    { 'path' : static_path })
-                ,(r'/.*$', NoDestinationHandler)
-            )
+            (r'/', Index),
+            (r'/feeds/edit/([0-9]+)/?', FeedEdit),
+            (r'/feeds/delete/([0-9]+)/?', FeedDelete),
+            (r'/feeds/add/?', FeedAdd),
+            (r'/refresh/?', Refresh),
+            (r'/mark_as_read/?', MarkAsRead),
+            (r'/static/(.*)',
+             tornado.web.StaticFileHandler,
+             {'path': static_path}),
+            (r'/.*$', NoDestinationHandler)
+        )
         settings = dict(
-                title = 'Huluobo',
-                debug = isfile(join(dirname(__file__), 'debug')),
-                xsrf_cookies = True,
-                static_path = static_path,
-                cookie_secret = cookie_secret
-            )
+            title='Huluobo',
+            debug=isfile(join(dirname(__file__), 'debug')),
+            xsrf_cookies=True,
+            static_path=static_path,
+            cookie_secret=cookie_secret
+        )
 
         template_path = join(dirname(__file__), 'templates')
         tornado.web.Application.__init__(self, handlers, **settings)
-        self.env = Environment(loader=FileSystemLoader(template_path)
-                ,extensions=['jinja2.ext.i18n'])
+        self.env = Environment(loader=FileSystemLoader(template_path),
+                               extensions=['jinja2.ext.i18n'])
         self.env.install_null_translations(newstyle=True)
         self.env.globals['cfg'] = settings
 
@@ -53,7 +56,11 @@ def main():
     http_server = tornado.httpserver.HTTPServer(Application())
     http_server.listen(options.port, '127.0.0.1')
     print('Starting Tornado on http://localhost:%d/' % options.port)
-    tornado.ioloop.IOLoop.instance().start()
+    try:
+        tornado.ioloop.IOLoop.instance().start()
+    except KeyboardInterrupt:
+        sys.exit(0)
+
 
 if __name__ == '__main__':
     main()
